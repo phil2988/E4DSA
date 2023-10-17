9e9c5931a4e930eafed375368eedf91da5f2c51f
Phillip Blicher <pfbb@blackcapitaltechnology.com>
2023-10-17 17:35:49 +0200

    Task 1 done

--- Case 2/main.m
+++ Case 2/main.m
@@ -1,7 +1,7 @@
 clc; clear;
 
 %% TASK 1
-% Generate the FSK Signal
+% part 1
 fstart = 1000; % transmission band frequency start
 fend = 5000; % transmission band frequency end
 Tsymbol = 0.5; % symbol duration in seconds
@@ -31,7 +31,76 @@ xlabel("Time(s)", 'FontSize', 16)
 xlim([1.1, 1.105])
 ylim([-1.5, 1.5])
 
+
+% part 3
 f2 = figure;
 spectrogram(signal, 1024, 512, 512, fs, "yaxis")
 title("Spectrogram for the FSK-Signal", 'FontSize', 16)
+
+% part 4.1
+signal_low_fstart = FSKgenerator('phillip', 100, fend, Tsymbol, fs);
+signal_high_fstart = FSKgenerator('phillip', 10000, fend, Tsymbol, fs);
+
+f3 = figure;
+spectos_fstart = tiledlayout(1, 2);
+title(spectos_fstart , " Spectrograms for the FSK-Signal", 'FontSize', 28)
+
+spec1 = nexttile;
+spectrogram(signal_low_fstart, 1024, 512, 512, fs, "yaxis")
+title("Spectrogram for the FSK-Signal - Low f-start", 'FontSize', 16)
+
+spec2 = nexttile;
+spectrogram(signal_high_fstart, 1024, 512, 512, fs, "yaxis")
+title("Spectrogram for the FSK-Signal - High f-start", 'FontSize', 16)
+
+% part 4.2
+signal_low_fend = FSKgenerator('phillip', fstart, 500, Tsymbol, fs);
+signal_high_fend = FSKgenerator('phillip', fstart, 50000, Tsymbol, fs);
+
+f4 = figure;
+spectos_fend = tiledlayout(1, 2);
+title(spectos_fend , " Spectrograms for the FSK-Signal", 'FontSize', 28)
+
+spec3 = nexttile;
+spectrogram(signal_low_fend, 1024, 512, 512, fs, "yaxis")
+title("Spectrogram for the FSK-Signal - Low f-end", 'FontSize', 16)
+
+spec4 = nexttile;
+spectrogram(signal_high_fend, 1024, 512, 512, fs, "yaxis")
+title("Spectrogram for the FSK-Signal - High f-end", 'FontSize', 16)
+
+% part 4.3
+signal_low_tsymbol = FSKgenerator('phillip', fstart, fend, 0.05, fs);
+signal_high_tsymbol = FSKgenerator('phillip', fstart, fend, 5, fs);
+
+f5 = figure;
+spectos_tsymbol = tiledlayout(1, 2);
+title(spectos_tsymbol , " Spectrograms for the FSK-Signal", 'FontSize', 28)
+
+spec5 = nexttile;
+spectrogram(signal_low_tsymbol, 1024, 512, 512, fs, "yaxis")
+title("Spectrogram for the FSK-Signal - Low t-symbol", 'FontSize', 16)
+
+spec6 = nexttile;
+spectrogram(signal_high_tsymbol, 1024, 512, 512, fs, "yaxis")
+title("Spectrogram for the FSK-Signal - High t-symbol", 'FontSize', 16)
+
+% part 4.4
+signal_low_fs = FSKgenerator('phillip', fstart, fend, Tsymbol, 2000);
+signal_high_fs = FSKgenerator('phillip', fstart, fend, Tsymbol, 200000);
+
+f6 = figure;
+spectos_fs = tiledlayout(1, 2);
+title(spectos_fs , " Spectrograms for the FSK-Signal", 'FontSize', 28)
+
+spec6 = nexttile;
+spectrogram(signal_low_fs, 1024, 512, 512, fs, "yaxis")
+title("Spectrogram for the FSK-Signal - Low fs", 'FontSize', 16)
+
+spec7 = nexttile;
+spectrogram(signal_high_fs, 1024, 512, 512, fs, "yaxis")
+title("Spectrogram for the FSK-Signal - High fs", 'FontSize', 16)
+
+
+
 
